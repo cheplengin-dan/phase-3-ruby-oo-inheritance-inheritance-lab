b3729c6960ff97318d59ac86bdeeeeaf0472f414
duncan kiptoo <cheplenginduncan@gmail.com>
2023-05-22 18:46:41 +0300

    Done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require_relative 'user'
 
+class Student < User
+  def initialize(first_name = nil, last_name = nil)
+    super(first_name, last_name)
+    @knowledge = []
+  end
+
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
+
